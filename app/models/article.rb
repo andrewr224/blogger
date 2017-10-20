dd3f63c82bf6af69c4ad33210dba314567fa7666
andrewr224 <andrew.r224@gmail.com>
2017-10-20 10:12:12 +0300

    Add tagging feature

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,14 @@
 class Article < ApplicationRecord
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
+
+  def tag_list
+    tags.join(", ")
+  end
+
+  def tag_list=(tag_string)
+    tag_names = tag_string.split(",").collect{ |tag| tag.strip.downcase }.uniq
+    new_or_found_tags = tag_names.collect { |name| Tag.find_or_create_by(name: name) }
+    self.tags = new_or_found_tags
+  end
 end
